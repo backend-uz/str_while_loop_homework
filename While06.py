0875dc45384e50289cd253a6abba0826673eb804
user1 <backenduz@gmail.com>
2022-11-29 16:23:57 +0500

    consonant

--- While06.py
+++ While06.py
@@ -11,8 +11,9 @@ def main(s):
     c = 0
 
     while i < len(s):
-        if s[i] != 'a' and s[i] != 'e' and s[i] != 'i' and s[i] != 'o' and s[i] !='u' and s[i] != 'A' and s[i] != 'E' and s[i] != 'I' and s[i] != 'O' and s[i] != 'U':
-            c += 1
-        i += 1
+        if s[i].isalpha():
+            if s[i] != 'a' and s[i] != 'e' and s[i] != 'i' and s[i] != 'o' and s[i] !='u' and s[i] != 'A' and s[i] != 'E' and s[i] != 'I' and s[i] != 'O' and s[i] != 'U':
+                c += 1
+            i += 1
     return c
 print(main('CodeschoolUz'))
