34d430cde1f59e029fe6278bee5dacb5f779ff3d
user1 <backenduz@gmail.com>
2022-11-29 16:10:54 +0500

    even numb

--- While07.py
+++ While07.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    c = 0
+    # l = len(s)
+    while i < len(s):
+        if int(s[i])%2 == 0:
+            c += 1
+        i += 1
+    return c
+print(main("1234"))
