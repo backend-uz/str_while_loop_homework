32ff1df440f4d5579e28952164bf11d30aa4f643
user1 <backenduz@gmail.com>
2022-11-29 17:56:24 +0500

    odd numb

--- While08.py
+++ While08.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    c = 0
+    while i < len(s):
+        if int(s[i])%2 != 0:
+            c += 1
+        i += 1
+    return c
+print(main("8234"))
